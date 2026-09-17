c446ea253bdcc1bd2ada107af7cd1e8b28cfd3ce
Dinesh-Betha <dineshbetha973@gmail.com>
2026-09-17 20:47:24 +0530

    Time: 22 ms (60.10%) | Memory: 22 MB (69.95%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findContentChildren(self, g: list[int], s: list[int]) -> int:
+        g.sort()
+        s.sort()
+        count,i,j = 0,0,0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                count += 1
+                i += 1
+            j += 1
+        return count
+        
